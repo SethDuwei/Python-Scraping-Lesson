d26e016d9903b1c6dd76809b005836c31c7a0e47
Seth <seth.duwei@gmail.com>
2019-02-27 12:20:11 +0800

    updats

--- application.py
+++ application.py
@@ -1,19 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-@app.route("/")
+notes = ["duwei", "hi"]
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        note = request.form.get("note")
+        notes.append(note)
+
+    return render_template("index.html", notes=notes)
 
 
 @app.route("/more")
 def more():
     return render_template("more.html")
-
-
-@app.route("/hello", methods=["POST"])
-def hello():
-    name = request.form.get("name")
-    return render_template("hello.html", name=name)
